3580be5d8f33a4e7f1fca05d99291348dd8ecca3
AashishBadal <aashishbadal4@gmail.com>
2025-10-19 22:06:39 +0545

    resume builder page update

---
+++ src/components/PersonalInfoForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const PersonalInfoForm = ({ data, onChange, removeBackground, setRemoveBackground }) => {
+    return (
+        <div>
+            <h3 className='text-lg font-semibold text-gray-900'>Personal Information</h3>
+            <p className='text-sm text-gray-600'>Get Started with the personal information</p>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default PersonalInfoForm
